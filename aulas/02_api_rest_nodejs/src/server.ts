0bb23c8c420de0fceed69b8faad192587b13973f
Thiago <poggianth@gmail.com>
2024-07-25 18:14:01 -0300

    02 - configurando o knex e criando a primeira migration

--- aulas/02_api_rest_nodejs/src/server.ts
+++ aulas/02_api_rest_nodejs/src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 const port = 3333
 // GET, POST, PUT, PATCH, DELETE
-app.get('/', () => {
-  return 'Hello World!'
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
